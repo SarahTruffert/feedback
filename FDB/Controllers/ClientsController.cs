c64ae709fd70c4692f1916c76ea668357f873eb1
sarahTruffert <sarah.truffertgf2015@gmail.com>
2022-03-22 12:22:51 +0100

    wip

---
+++ FDB/Controllers/ClientsController.cs
@@ -0,0 +1,120 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FDB.DataContext;
+using FDB.Mod;
+
+namespace FDB.Controllers
+{
+    [Route("api/[controller]")]
+    [RestrictDomain("https://feedback-webapp.azurewebsites.net")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        private readonly FDBContext _context;
+
+        public ClientsController(FDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ClientsDto>>> GetClientsDto()
+        {
+            return await _context.ClientsDto.ToListAsync();
+        }
+
+       
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ClientsDto>> GetClientsDto(Guid id)
+        {
+            var clientsDto = await _context.ClientsDto.FindAsync(id);
+
+            if (clientsDto == null)
+            {
+                return NotFound();
+            }
+
+            return clientsDto;
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutClientsDto(Guid id, ClientsDto clientsDto)
+        {
+            if (id != clientsDto.User_Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(clientsDto).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClientsDtoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ClientsDto>> PostClientsDto(ClientsDto clientsDto)
+        {
+            _context.ClientsDto.Add(clientsDto);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (ClientsDtoExists(clientsDto.User_Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetClientsDto", new { id = clientsDto.User_Id }, clientsDto);
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteClientsDto(Guid id)
+        {
+            var clientsDto = await _context.ClientsDto.FindAsync(id);
+            if (clientsDto == null)
+            {
+                return NotFound();
+            }
+
+            _context.ClientsDto.Remove(clientsDto);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ClientsDtoExists(Guid id)
+        {
+            return _context.ClientsDto.Any(e => e.User_Id == id);
+        }
+    }
+}
---
+++ FDB/Controllers/PublicationsController.cs
@@ -0,0 +1,124 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FDB.DataContext;
+using FDB.Mod;
+
+namespace FDB.Controllers
+{
+    [Route("api/[controller]")]
+    [RestrictDomain("https://feedback-webapp.azurewebsites.net")]
+    [ApiController]
+    public class PublicationsController : ControllerBase
+    {
+        private readonly FDBContext _context;
+
+        public PublicationsController(FDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Publications
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PublicationsDto>>> GetPublicationDto()
+        {
+            return await _context.PublicationDto.ToListAsync();
+        }
+
+        // GET: api/Publications/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PublicationsDto>> GetPublicationsDto(Guid id)
+        {
+            var publicationsDto = await _context.PublicationDto.FindAsync(id);
+
+            if (publicationsDto == null)
+            {
+                return NotFound();
+            }
+
+            return publicationsDto;
+        }
+
+        // PUT: api/Publications/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPublicationsDto(Guid id, PublicationsDto publicationsDto)
+        {
+            if (id != publicationsDto.Publication_Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(publicationsDto).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PublicationsDtoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Publications
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<PublicationsDto>> PostPublicationsDto(PublicationsDto publicationsDto)
+        {
+            _context.PublicationDto.Add(publicationsDto);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (PublicationsDtoExists(publicationsDto.Publication_Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetPublicationsDto", new { id = publicationsDto.Publication_Id }, publicationsDto);
+        }
+
+        // DELETE: api/Publications/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePublicationsDto(Guid id)
+        {
+            var publicationsDto = await _context.PublicationDto.FindAsync(id);
+            if (publicationsDto == null)
+            {
+                return NotFound();
+            }
+
+            _context.PublicationDto.Remove(publicationsDto);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PublicationsDtoExists(Guid id)
+        {
+            return _context.PublicationDto.Any(e => e.Publication_Id == id);
+        }
+    }
+}
